7aff15568bb608c4322d699cf1b6aa9a43162982
Reihan Mursyidi <reihanmursyidi21@gmail.com>
2026-08-09 08:15:47 +0700

    Change DATABASE_URL to POSTGRES_URL in database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("POSTGRES_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
